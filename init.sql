4424f2bc6b141589fa3009edc41bf1c7330788cb
Ignacio Sanchez <sanchezih@gmail.com>
2023-10-25 17:24:31 -0300

    Create init.sql

---
+++ init.sql
@@ -0,0 +1,66 @@
+USE ownblogdb;
+
+INSERT INTO publicacion (titulo, descripcion, contenido) VALUES (
+	'Los mejores juegos para iPhone',
+	'Los usuarios pueden...',
+	'Los usuarios pueden descargar estas aplicaciones de forma gratuita desde la App Store...');
+
+INSERT INTO publicacion (titulo, descripcion, contenido) VALUES (
+	'Las recomendaciones de Google Flights',
+	'Se aconseja a los interesados...',
+	'Se aconseja a los interesados que programen sus viajes con varios dias de anticipacion para...');
+
+INSERT INTO publicacion (titulo, descripcion, contenido) VALUES (
+	'Formatos JPG, PNG, GIF y TIFF',
+	'Estos conceptos...',
+	'Estos conceptos se ven siempre que se desea almacenar un archivo...');
+	
+INSERT INTO publicacion (titulo, descripcion, contenido) VALUES (
+	'Logitech K835TKL',
+	'Hubo una epoca en la que...',
+	'Hubo una epoca en la que los teclados mecanicos eran para gamers y venian con luces...');
+	
+INSERT INTO publicacion (titulo, descripcion, contenido) VALUES (
+	'Quake III Arena',
+	'Quake III Arena en Full HD...',
+	'Quake III Arena en Full HD, el grandisimo FPS sigue dando pelea...');
+	
+INSERT INTO publicacion (titulo, descripcion, contenido) VALUES (
+	'IA gana concurso de arte',
+	'Obra generada por IA gana concurso de arte...',
+	'El primer premio del concurso de arte, en la categoria Artes...');
+
+INSERT INTO publicacion (titulo, descripcion, contenido) VALUES (
+	'Robots policias patrullan Tokio',
+	'Hace unos dias el primer...',
+	'Hace unos dias el primer grupo de robots policias comenzo a patrullar el centro de Tokio...');
+	
+INSERT INTO publicacion (titulo, descripcion, contenido) VALUES (
+	'Musk vende acciones de Tesla',
+	'Musk vende acciones de Tesla, pero afirma que es el momento de comprar...',
+	'Entre el 5 y el 9 de agosto Elon Musk vendio unos USD 7 mil millones en acciones de Tesla...');
+	
+INSERT INTO publicacion (titulo, descripcion, contenido) VALUES (
+	'Conseguir criptomonedas',
+	'Los 5 metodos mas efectivos para conseguir criptomonedas',
+	'En el articulo no se menciona la mineria de criptomonedas, puesto que supone un importante gasto energetico...');
+
+INSERT INTO publicacion (titulo, descripcion, contenido) VALUES (
+	'13 juegos mas para PC gratis',
+	'Este fin de semana llegan 13 juegos mas para PC gratis',
+	'Se esta volviendo una costumbre que las plataformas de videojuegos en linea habiliten algunos titulos para probar gratis...');
+	
+INSERT INTO publicacion (titulo, descripcion, contenido) VALUES (
+	'Crear avatares',
+	'Los 5 sitios web para crear avatares',
+	'Los avatares son las representacion grafica de los usuarios que estan ingresando al metaverso...');
+
+INSERT INTO publicacion (titulo, descripcion, contenido) VALUES (
+	'Escritorio remoto',
+	'Escritorio remoto, la opcion de Windows para...',
+	'Con solo entrar a las configuraciones del sistema operativo se podra manejar la computadora desde otro dispositivo...');
+
+INSERT INTO publicacion (titulo, descripcion, contenido) VALUES (
+	'Hacer ejercicio',
+	'3 aplicaciones para hacer ejercicio...',
+	'Las plataformas pueden ser gratuitas o tener opciones de pago para los usuarios que deseen rutinas...');
